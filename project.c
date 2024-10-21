c1698ce22b0aef43d25190371049c7b25f15b624
berdery <ambre.berdery@gmail.com>
2024-10-21 17:10:58 +0200

    suppression files

--- project.c
+++
@@ -1,3 +0,0 @@
-//
-// Created by ambre on 21/10/2024.
-//
--- project.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by ambre on 21/10/2024.
-//
-
-#ifndef ROVER_RUN_PROJECT_PROJECT_H
-#define ROVER_RUN_PROJECT_PROJECT_H
-
-#endif //ROVER_RUN_PROJECT_PROJECT_H
-
