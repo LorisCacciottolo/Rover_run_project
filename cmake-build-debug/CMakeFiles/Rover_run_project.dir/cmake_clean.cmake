99d8af76674443b1a33c23ba69640829a10841a5
LorisCacciottolo <loriscacciottolo@gmail.com>
2024-11-13 10:47:35 +0100

    random files

---
+++ cmake-build-debug/CMakeFiles/Rover_run_project.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rover_run_project.dir/MARC-master/loc.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/loc.c.obj.d"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/main.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/main.c.obj.d"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/map.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/map.c.obj.d"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/moves.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/moves.c.obj.d"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/queue.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/queue.c.obj.d"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/random.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/random.c.obj.d"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/stack.c.obj"
+  "CMakeFiles/Rover_run_project.dir/MARC-master/stack.c.obj.d"
+  "Rover_run_project.exe"
+  "Rover_run_project.exe.manifest"
+  "Rover_run_project.pdb"
+  "libRover_run_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rover_run_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Rover_run_project.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Rover_run_project.
+# This may be replaced when dependencies are built.
