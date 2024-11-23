0fc44693e26fef7e2fade4df4e363ee4626e4b7a
berdery <ambre.berdery@gmail.com>
2024-11-23 16:56:16 +0100

    replace main.c

--- MARC-master/main.c
+++ MARC-master/main.c
@@ -12,87 +12,11 @@
 #define MAX_PATH_LENGTH 6
 
 
-void printPath(const char *prefix, const char *string) {
-    printf("%s ", prefix);
-    if (string != NULL)
-        printf("%s", string);
-    else
-        printf("(null)");
-    printf("\n");
-}
-
-char* arrayrandomproba() {
-    char *arrayrandomproba = (char*)malloc((MAX_CHILDREN + 1) * sizeof(char));
-    if (arrayrandomproba == NULL) {
-        return NULL;
-    }
-
-    int SIZE = 100;
-    int F_10 = 22, F_20 = 15, F_30 = 7;
-    int R_10 = 7, TR = 21, TL = 21, TB = 7;
-
-    srand(time(0));
-
-    char arrayloop[SIZE];
-    int index = 0;
-
-    for (int i = 0; i < F_10; i++) arrayloop[index++] = 'A';
-    for (int i = 0; i < F_20; i++) arrayloop[index++] = 'B';
-    for (int i = 0; i < F_30; i++) arrayloop[index++] = 'C';
-    for (int i = 0; i < R_10; i++) arrayloop[index++] = 'R';
-    for (int i = 0; i < TR; i++) arrayloop[index++] = 'T';
-    for (int i = 0; i < TL; i++) arrayloop[index++] = 'L';
-    for (int i = 0; i < TB; i++) arrayloop[index++] = 'J';
-
-    for (int i = 0; i < 9; i++) {
-        int randomnumber = rand() % SIZE;
-        arrayrandomproba[i] = arrayloop[randomnumber];
-
-        switch (arrayrandomproba[i]) {
-            case 'A': F_10--; break;
-            case 'B': F_20--; break;
-            case 'C': F_30--; break;
-            case 'R': R_10--; break;
-            case 'T': TR--; break;
-            case 'L': TL--; break;
-            case 'J': TB--; break;
-        }
-
-        for (int j = randomnumber; j < SIZE - 1; j++) {
-            arrayloop[j] = arrayloop[j + 1];
-        }
-        SIZE--;
-    }
-
-    arrayrandomproba[MAX_CHILDREN] = '\0';
-    return arrayrandomproba;
-}
-
-// Struct of tree
-typedef struct t_node {
-    char path[MAX_PATH_LENGTH];
-    int val;
-    struct t_node *children[MAX_CHILDREN];
-} t_node;
-
-
-// Struct of tree root
-typedef struct s_tree
-{
-    t_node *root;
-} t_tree;
-
-t_tree createEmptyTree(){
-    t_tree temp;
-    temp.root = NULL;
-    return temp;
-}
-
-
-
-
 int main(){
-
+    //initialising the map
+    t_map map;
+    displayMap(map);
+    t_localisation robot = initialRobotPosition(map);
     return 0;
 }
 
